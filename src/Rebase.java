aeb6f83edff0cc5ee9117597de9f619babc50e5a
tianjunjie <tianjunjie@bytedance.com>
2022-03-26 10:44:43 +0800

    add commit3
    Change-Id: I2564654a9939c505cfe44769395c53a1cdc44929

--- src/Rebase.java
+++ src/Rebase.java
@@ -2,6 +2,7 @@ public class Rebase {
     public void testCommit() {
         System.out.println("branch2_commit1");
         System.out.println("branch2_commit2");
+        System.out.println("branch1_commit3");
     }
     public static void main(String[] args) {
 
