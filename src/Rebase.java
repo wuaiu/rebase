6bd8baf3d41d729624b78dda2bf1838d6b50bde1
tianjunjie <tianjunjie@bytedance.com>
2022-03-26 10:44:33 +0800

    add commit2
    Change-Id: I0175a809778340003195ea3fd9d22802d0c61ac9

--- src/Rebase.java
+++ src/Rebase.java
@@ -1,6 +1,7 @@
 public class Rebase {
     public void testCommit() {
         System.out.println("branch2_commit1");
+        System.out.println("branch2_commit2");
     }
     public static void main(String[] args) {
 
