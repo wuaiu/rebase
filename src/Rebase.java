ee9ccd701e7fb33d5320a82aad9a292ba62fff92
tianjunjie <tianjunjie@bytedance.com>
2022-03-26 10:44:21 +0800

    add commit branch2
    Change-Id: I5123c9f5fd491f79ffb90d6a9515b57acfcafb21

--- src/Rebase.java
+++ src/Rebase.java
@@ -1,6 +1,9 @@
 public class Rebase {
     public void testCommit() {
         System.out.println("branch1_commit1");
+        System.out.println("branch2_commit1");
+        System.out.println("branch2_commit2");
+        System.out.println("branch1_commit3");
     }
     public static void main(String[] args) {
 
